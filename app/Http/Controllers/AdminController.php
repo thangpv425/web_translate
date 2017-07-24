66c38f6cf6c2b3a1283c632c533b469053738c9b
01101100 <long.dragon96@gmail.com>
2017-07-24 17:47:29 +0700

    nothing changed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,15 +6,26 @@ use Illuminate\Http\Request;
 use App\KeywordTemp;
 use App\MeaningTemp;
 use App\Users;
+use App\keyword;
 
 class AdminController extends Controller
 {
+	/**
+	 * return List request of keyword table
+	 * @return [type] [description]
+	 */
     public function keywordTempList()
     {
     	$data = KeywordTemp::all();
     	return view('admin.approve.keyword.all', ['data' => $data]);
     }
 
+    /**
+     * Approve request on keyword table
+     * @param  [type] $id     [description]
+     * @param  [type] $opCode [description]
+     * @return [type]         [description]
+     */
     public function keywordApprove($id, $opCode)
     {
     	// echo $id;
@@ -22,14 +33,20 @@ class AdminController extends Controller
     	if($data != null){
     		switch ($opCode) {
 	    		case '0':
-	    			echo "adding";
-
+	    			$keyword = new keyword;
+	    			$keyword->value = $data['new_keyword'];
+	    			$keyword->status = 1;
+	    			$keyword->save();
+	    			$data->delete();
 	    			break;
 	    		case '1':
-	    			echo "editing";
+	    			$keyword = keyword::find($data['old_keyword_id']);
+	    			$keyword->value = $data['new_keyword'];
+	    			$keyword->save();
+	    			$data->delete();
 	    			break;
 	    		case '2':
-	    			echo "deleting";;
+	    			
 	    			break;	
 	    		default:
 
@@ -40,11 +57,20 @@ class AdminController extends Controller
     	return redirect()->back();
     }
 
+    /**
+     * Decline request on keyword table
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
     public function keywordDecline(Request $request)
     {
     	echo $request->get('id');
     }
 
+    /**
+     * return list request on meaning table
+     * @return [type] [description]
+     */
     public function meaningTempList()
     {
     	$data = MeaningTemp::all();
