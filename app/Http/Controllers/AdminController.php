2c9dd989518feb25d89b4ebf490c47337ea8dfe1
manhnvit <manhnvit@gmail.com>
2017-07-26 11:22:46 +0700

    Edit user validate and delete user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -203,4 +203,11 @@ class AdminController extends Controller
         return redirect('admin/show')->with('notification','You have successfully added the user');
     }
 
+    public function delete($id = NULL)
+    {
+        $user = Users::find($id);
+        $user->delete();
+        return redirect('admin/show')->with('notification','You have successfully deleted the user');
+    }
+
 }
