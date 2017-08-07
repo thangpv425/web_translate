b9ed72c8f678fdf7a638d274e746b7328136b665
thanhthudhbkhn <thanhthuk59@gmail.com>
2017-08-07 14:48:14 +0700

    nothing change

--- app/MeaningTemp.php
+++ app/MeaningTemp.php
@@ -3,15 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MeaningTemp extends Model {
 
-    use SoftDeletes;
-
     protected $table = 'wt_meaning_temp';
-    public $primaryKey = 'meaning_temp_id';
-    protected $dates = ['deleted_at'];
+    public $primaryKey = 'id';
 
     public function user() {
         return $this->belongsTo('App\User');
